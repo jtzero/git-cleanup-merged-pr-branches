daaf1689f4f65c99aff723dc4d7216de4c380949
jtzero ? <jtzero511@gmail.com>
2023-01-25 17:15:12 -0500

    + azure menu when no-subscriptions devops-ops

--- lib/platforms/azure.sh
+++ lib/platforms/azure.sh
@@ -1,17 +1,108 @@
 #!/usr/bin/env bash
 
+AZ_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}/az/token"
+
 # shellcheck disable=SC2034
 COMPLETED_STATES=('completed' 'abandoned')
 
+# Renders a text based list of options that can be selected by the
+# user using up, down and enter keys and returns the chosen option.
+#
+#   Arguments   : list of options, maximum of 256
+#                 "opt1" "opt2" ...
+#   Return value: selected index (0 for opt1, 1 for opt2 ...)
+select_option() {
+
+  # little helpers for terminal print control and key input
+  ESC=$( printf "\033")
+  cursor_blink_on()  { printf "$ESC[?25h"; }
+  cursor_blink_off() { printf "$ESC[?25l"; }
+  cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+  print_option()     { printf "   $1 "; }
+  print_selected()   { printf "  $ESC[7m $1 $ESC[27m"; }
+  get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+  key_input()        { read -s -n3 key 2>/dev/null >&2
+                       if [[ $key = $ESC[A ]]; then echo up;    fi
+                       if [[ $key = $ESC[B ]]; then echo down;  fi
+                       if [[ $key = ""     ]]; then echo enter; fi; }
+
+  # initially print empty new lines (scroll down if at bottom of screen)
+  for opt; do printf "\n"; done
+
+  # determine current screen position for overwriting the options
+  local lastrow=`get_cursor_row`
+  local startrow=$(($lastrow - $#))
+
+  # ensure cursor and input echoing back on upon a ctrl+c during read -s
+  trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+  cursor_blink_off
+
+  local selected=0
+  while true; do
+      # print options by overwriting the last lines
+      local idx=0
+      for opt; do
+          cursor_to $(($startrow + $idx))
+          if [ $idx -eq $selected ]; then
+              print_selected "$opt"
+          else
+              print_option "$opt"
+          fi
+          ((idx++))
+      done
+
+      # user key control
+      case `key_input` in
+          enter) break;;
+          up)    ((selected--));
+                 if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+          down)  ((selected++));
+                 if [ $selected -ge $# ]; then selected=0; fi;;
+      esac
+  done
+
+  # cursor position back to normal
+  cursor_to "${lastrow}"
+  printf "\n"
+  cursor_blink_on
+
+
+  return $selected
+}
+
+ask_for_pat() {
+  local config_folder="${1}"
+  read -s -p "Token:" token
+  mkdir -p "${config_folder}"
+  printf '%s' "${token}" > "${config_folder}/token"
+}
+
 pre_init_hook() {
+  local -r spinner_pid="${1}"
+
+  local -r cached_pat="$(<"${AZ_CONFIG_HOME}/token")"
+  AZURE_DEVOPS_EXT_PAT="${AZURE_DEVOPS_EXT_PAT:-${cached_pat}}"
+
   if [ -z "${AZURE_DEVOPS_EXT_PAT:-}" ]; then
     set +e
-    local exit_code="0"
-    local -r status="$(az account show 2>&1)"
+    local exit_code="0", command_output=""
+    command_output="$(az account show 2>&1)"
     exit_code="$?"
-    set -e
     if [ "${exit_code}" != "0" ]; then
-      az login || exit 1
+      if kill -s 0 "${spinner_pid}" >/dev/null 2>&1; then
+        kill "${spinner_pid}"
+      fi
+      local options=("PAT" "AZ Web SSO")
+      ( exec </dev/tty; exec 1>&2; select_option "${options[@]}" )
+      choice=$?
+      if [ "${choice}" = "0" ]; then
+        ( exec </dev/tty; ask_for_pat "${AZ_CONFIG_HOME}" )
+        AZURE_DEVOPS_EXT_PAT="$(<"${AZ_CONFIG_HOME}/token")"
+        az devops login | exit 1
+      else
+        az login || exit 1
+      fi
+      set -e
     fi
   fi
 }
